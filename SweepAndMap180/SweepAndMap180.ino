9bd235941800cf77f83630ae04686e2f90c0e0f5
Matthew Jilk <55640779+matthewj12@users.noreply.github.com>
2022-01-08 15:10:27 -0600

    Update SweepAndMap180.ino

--- SweepAndMap180/SweepAndMap180.ino
+++ SweepAndMap180/SweepAndMap180.ino
@@ -1,71 +1,41 @@
 #include <Servo.h>
 #include <LIDARLite.h>
 #include <Wire.h>
-#include <Vector.h>
 
 // https://learn.sparkfun.com/tutorials/lidar-lite-v3-hookup-guide/all
 
-class Point3D {
-private:
-    float m_x, m_y, m_z;
-public:
-	Point3D() {
-	    m_x = 0;
-        m_y = 0;
-        m_z = 0;
-    }
-    
-    Point3D(float p_x, float p_y, float p_z) {
-        m_x = p_x;
-        m_y = p_y;
-        m_z = p_z;
-    }
-    
-    float getX() { return m_x; }
-    float getY() { return m_y; }
-    float getZ() { return m_z; }
-
-    void setX(float p_x) { m_x = p_x; }
-    void setY(float p_y) { m_y = p_y; }
-    void setZ(float p_z) { m_z = p_z; }
-    
-};
-
-float degreesToRadians(float p_deg_val) {
-    return p_deg_val * 0.0174533;
-}
+const int servo_range = 180;
+const int pan_intvl  = (int) (servo_range / 20);
+const int tilt_intvl = (int) (servo_range / 20);
 
 void sweepAndMap(Servo& pan_servo, Servo& tilt_servo) {
     pan_servo.write(0);
     delay(200);
     
-    for (int pan_pos = 0; pan_pos <= 180; pan_pos += 10) {
+    for (int pan_pos = 0; pan_pos <= 180; pan_pos += pan_intvl) {
         pan_servo.write(pan_pos);
         delay(20);
         tilt_servo.write(0);
         delay(200);
         
-        for (int tilt_pos = 0; tilt_pos <= 90; tilt_pos += 10) {
+        for (int tilt_pos = 0; tilt_pos <= 180; tilt_pos += tilt_intvl) {
             tilt_servo.write(tilt_pos);
             delay(20);
             
             float dist = lidar.distance(true);
-            float pan_pos_rad = degreesToRadians(pan_pos);
-            float tilt_pos_rad = degreesToRadians(tilt_pos);
-            float xy_dist = cos(tilt_pos_rad) * dist; // xy component of dist
 
-            Serial.print(cos(pan_pos_rad) * xy_dist);
+            Serial.print(dist);
             Serial.print(", ");
-            Serial.print(sin(pan_pos_rad) * xy_dist);
+            Serial.print(pan_pos);
             Serial.print(", ");
-            Serial.print(sin(tilt_pos_rad) * dist);
+            Serial.print(tilt_pos);
             Serial.println();
         }
     }
 }
+
 Servo pan_servo, tilt_servo;
 LIDARLite lidar;
-int cal_cnt = 0;
 
 void setup() {
     Serial.begin(9600);
